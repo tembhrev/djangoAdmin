99333d718e7a1fe7c8fe5d5aecdd4029adbffcc4
tembhrev <sw.vikastembhre@gmail.com>
2024-04-10 19:56:45 +0530

    add search fields in admin.py

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -7,6 +7,8 @@ class MovieAdmin(admin.ModelAdmin):
     
     fields = ['release_year', 'title', 'length']
     
+    search_fields = ['title', 'length']
+    
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
